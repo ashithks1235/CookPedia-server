d34e72786effce18818e1faaad3a20ad9dbb6e5f
Ashith K Sasi <ashith@Ashiths-MacBook-Air.local>
2026-02-04 12:26:01 +0530

    download and save model

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const jwtMiddleware = (req,res,next)=>{
     console.log("inside jwtMiddleware");
-    const token = req.headers['authorization'].split("")[1]
+    const token = req.headers['authorization'].split(" ")[1]
     if(token){
         try{
             const jwtResponse = jwt.verify(token,process.env.JWT_SECRET_KEY)
--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const recipeController = require('./controller/recipeController')
 const userController = require('./controller/userController')
-
+const downloadController = require("./controller/downloadController")
+const saveRecipeController = require("./controller/saveRecipeController")
+const jwtMiddleware = require('./middlewares/jwtMiddleware')
 const router = new express.Router()
 
 //routes
@@ -19,5 +21,14 @@ router.get('/recipes/:id',recipeController.viewRecipeController)
 
 //related recipe
 router.get('/recipes-related',recipeController.relatedRecipeController)
+
+//download recipe
+router.post('/downloads/:id',jwtMiddleware,downloadController.addToDownloadController)
+
+//save recipe
+router.post('/save-recipe/:id',jwtMiddleware,saveRecipeController.saveRecipeToCollectionController)
+
+//get save recipe
+router.get('/save-recipes',jwtMiddleware,saveRecipeController.getUserSavedCollectionController)
 
 module.exports = router
